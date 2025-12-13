83d47f00b32f4b855cdf7c47867feb07ce9b3910
Benjamin Snow <benjamin.snow@prometric.com>
2025-12-13 11:15:54 -0600

    Enhance navigation setup with favicon and manifest files; update logo component to accept custom logo

--- src/shared/components/logo/logo.jsx
+++ src/shared/components/logo/logo.jsx
@@ -1,10 +1,10 @@
 import reactLogo from '@shared/assets/images/react-logo.svg';
 import styles from './logo.module.less';
 
-function Logo() {
+function Logo({ logo }) {
   return (
     <div className={styles.logoContainer}>
-      <img src={reactLogo} alt="React Logo" className={styles.logo} />
+      <img src={logo || reactLogo} alt="Logo" className={styles.logo} />
     </div>
   )
 }
