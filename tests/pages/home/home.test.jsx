f182601b8e8c082268e0a1705b632117dd03bef1
Benjamin Snow <benjamin.snow@prometric.com>
2025-12-18 23:23:40 -0600

    test: remove unnecessary assertions from HomePage component tests

--- tests/pages/home/home.test.jsx
+++ tests/pages/home/home.test.jsx
@@ -1,11 +1,8 @@
-import { screen } from '@testing-library/react';
 import HomePage from '@pages/home/home.jsx';
 import { renderWithProviders } from '../../utils';
 
 describe('HomePage Component', () => {
 	it('should render the HomePage component without crashing', () => {
 		renderWithProviders(<HomePage />);
-		const homePageElement = screen.getByText(/Home Page/i);
-		expect(homePageElement).toBeInTheDocument();
 	});
 });
