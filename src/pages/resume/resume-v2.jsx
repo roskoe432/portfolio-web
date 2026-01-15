29d493f20ab49956203e1a04a6ff16bed755c7da
Benjamin Snow <benjamin.snow@prometric.com>
2026-01-15 16:33:52 -0600

    Feature: Enhance resume page with download button and styling adjustments

--- src/pages/resume/resume-v2.jsx
+++ src/pages/resume/resume-v2.jsx
@@ -5,13 +5,28 @@ pdfjs.GlobalWorkerOptions.workerSrc = pdfWorkerURL;
 
 import React from 'react';
 import { Document, Page } from 'react-pdf';
+import { Button } from '@mantine/core';
+import { useTranslation } from 'react-i18next';
 import resumePDF from '../../shared/assets/docs/resume.pdf';
+import styles from './resume.module.less';
 
 function ResumeV2() {
+	const { t } = useTranslation();
+
+	const handleDownload = () => {
+		const link = document.createElement('a');
+		link.href = resumePDF;
+		link.download = 'Benjamin_Snow_Resume.pdf';
+		link.click();
+	};
+
 	return (
-		<div
-			style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}
-		>
+		<div className={styles.resumeContainer}>
+			<div className={styles.controls}>
+				<Button color="grey" size="compact-xs" onClick={handleDownload}>
+					{t('common.download')}
+				</Button>
+			</div>
 			<Document file={resumePDF} options={{ workerSrc: pdfWorkerURL }}>
 				<Page
 					pageNumber={1}
