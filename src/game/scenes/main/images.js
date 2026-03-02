3c16119797d0395b605c11dc5518a7d0e8b83c19
Benjamin Snow <benjamin.snow@prometric.com>
2026-03-02 09:14:18 -0600

    Remove unused rotation animation images and add new walking animation frames for player character

---
+++ src/game/scenes/main/images.js
@@ -0,0 +1,14 @@
+const imagesConfig = [
+	{
+		name: 'player-idle',
+		path: 'animations/idle/south',
+		anim: {
+			frames: 4,
+			frameRate: 8,
+			repeat: -1,
+			prefix: 'frame_',
+		},
+	},
+];
+
+export default imagesConfig;
