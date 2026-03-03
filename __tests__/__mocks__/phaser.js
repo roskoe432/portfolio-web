f90ff7c7e993519be0b9b3b33e1edd63d4d848a2
Benjamin Snow <benjamin.snow@prometric.com>
2026-03-02 22:52:52 -0600

    feat: add MockEventEmitter class and EventBus for event handling

--- __tests__/__mocks__/phaser.js
+++ __tests__/__mocks__/phaser.js
@@ -27,9 +27,39 @@ class MockScene {
 	}
 }
 
+class MockEventEmitter {
+	constructor() {
+		this.events = {};
+	}
+
+	on(event, listener) {
+		if (!this.events[event]) {
+			this.events[event] = [];
+		}
+		this.events[event].push(listener);
+	}
+
+	emit(event, ...args) {
+		if (this.events[event]) {
+			this.events[event].forEach((listener) => listener(...args));
+		}
+	}
+
+	off(event, listener) {
+		if (this.events[event]) {
+			this.events[event] = this.events[event].filter((l) => l !== listener);
+		}
+	}
+}
+
+export const EventBus = new MockEventEmitter();
+
 export default {
 	Game: MockGame,
 	Scene: MockScene,
+	Events: {
+		EventEmitter: MockEventEmitter,
+	},
 	Scale: {
 		FIT: 'fit',
 		NONE: 'none',
